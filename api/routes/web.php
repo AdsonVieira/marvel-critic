bd4aa1d77def351a9fb0e4116f2daf2924d6c0ce
AdsonVieira <adsonvieira@live.com>
2022-06-19 15:15:30 -0300

    Single page básica com autenticação e outras funções

--- api/routes/web.php
+++ api/routes/web.php
@@ -14,5 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return response('Unauthorized.', 401);
+    return response()->json(['error' => 'Unauthorized.'], 401);
 });
